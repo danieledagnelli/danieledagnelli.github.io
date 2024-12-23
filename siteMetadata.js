be115d11b42aa5e0bcdd315ab4a0029cb56ff52f
Daniele D'Agnelli <daniele@bidimensional.net>
2024-12-23 19:13:15 +0000

    feat: enhance ContactSidebar and add ContactMain component with site metadata integration

---
+++ siteMetadata.js
@@ -0,0 +1,18 @@
+
+const siteMetadata = {
+  title: 'My Website',
+  description: 'A description of my website',
+  author: 'Author Name',
+  siteUrl: 'https://www.example.com',
+  social: {
+    twitter: 'example',
+    facebook: 'example',
+  },
+  contactSidebar: {
+    title: 'Contact Me',
+    phone: '+1-000-000-0000',
+    email: 'example@example.com'
+  },
+}
+
+module.exports = siteMetadata
--- src/siteMetadata.js
+++ src/siteMetadata.js
@@ -1,5 +1,16 @@
 export const siteMetadata = {
-    title: 'daniele d\'agnelli',
-    description: 'old school',
-  };
-  
+  title: 'daniele d\'agnelli',
+  description: 'old school',
+
+  name: "Daniele D'Agnelli",
+  github_handle: "danieledagnelli",
+  bsky_handle: "dagnelli.net",
+  linkedin_handle: "dagnelli",
+  statusEmoji: "🍕",
+  statusMessage: "probably eating pizza",
+  bio: "old school",
+  location: "London, UK",
+  email: "daniele@dagnelli.net",
+  website: "https://example.com",
+  orgs: []  // e.g., array of org logo URLs
+};
